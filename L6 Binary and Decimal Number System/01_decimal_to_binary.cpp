4f64b7dd5fb3c27163baae25e51f1e3fa518bf50
Devkumarsingh16 <sdev06829@gmail.com>
2026-05-31 14:59:20 +0530

    Add decimal to binary conversion program

---
+++ L6 Binary and Decimal Number System/01_decimal_to_binary.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    int ans = 0;
+    int place = 1;
+ 
+    while(n!=0){
+
+        int bit = n & 1;
+        ans = ans + bit * place;
+        place = place*10;
+
+        n = n >> 1;
+        
+    }
+    cout<<"answer is: "<<ans<<endl;
+
+    
+
+}
