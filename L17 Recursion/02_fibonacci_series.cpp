75cbd1f43ce0ea1bb6c76836b8b240f673ff5873
Devkumarsingh16 <sdev06829@gmail.com>
2026-06-13 15:23:12 +0530

    Implement Fibonacci series using recursion

---
+++ L17 Recursion/02_fibonacci_series.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+
+    int fib(int n) {
+     
+        //base case
+
+        if(n == 0){
+            return 0;
+        }
+
+        if(n == 1){
+            return 1;
+        }
+        
+          // recursive call
+       
+        if(n > 1)
+           return fib(n-1) + fib(n-2);  
+
+    }
+    int main(){
+
+        int n;
+        cout <<" Enter a positive integer :";
+        cin>>n;
+
+        for(int i = 0; i<=n;i++){
+            cout << fib(i) << " ";
+        }
+}
