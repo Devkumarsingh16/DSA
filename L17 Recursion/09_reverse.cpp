7bf2e9b8fb2b96c884884d6a28abf7313300c889
Devkumarsingh16 <sdev06829@gmail.com>
2026-06-15 16:50:14 +0530

    Add recursive string reversal function
    Implement a recursive function to reverse a string.

---
+++ L17 Recursion/09_reverse.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+void reverse(string& str,int i,int j){
+
+    // base case
+
+    if(i > j){
+        return ;
+    }
+
+    // processing
+    swap(str[i],str[j]);
+    i++;
+    j--;
+
+    // recursive call
+      reverse(str,i,j);
+}
+
+int main(){
+
+    string name = "babber"; 
+
+     reverse(name,0,name.length()-1);
+
+    cout << name <<endl;
+    return 0;
+}
